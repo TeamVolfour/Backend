aae0fe96baddf9792591ea9b9661e106897b7122
Geto1-creator <84434642+Geto1-creator@users.noreply.github.com>
2023-05-27 22:51:59 +0800

    final push 5-27

--- Model/certificateModel.js
+++ Model/certificateModel.js
@@ -1,6 +1,9 @@
 const { Schema, model, Types, SchemaTypes } = require("mongoose");
 const Certificate = new Schema({
-  holderFullName: String,
+  holderFullName: {
+    firstname: String,
+    lastname: String
+  },
   createdAt: { type: Date, default: Date.now() },
   jobId: { type: Schema.Types.ObjectId, ref: "JobPost" }
 });
